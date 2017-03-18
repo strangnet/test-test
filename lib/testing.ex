4dff63b01571e43982c501abf17188315fe9fe13
Patrick Strang <strangnet@gmail.com>
2017-03-18 13:08:00 +0100

    initial commit

---
+++ lib/testing.ex
@@ -0,0 +1,18 @@
+defmodule Testing do
+  @moduledoc """
+  Documentation for Testing.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Testing.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
